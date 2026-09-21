eac21b9fff8d980eb7881942b4406f0ee5d80355
Fred <frederikpriesshansen@gmail.com>
2026-09-21 11:04:13 +0200

    added comment to capitalize function in formatter.js
    added pokemonCard.js

--- javascript/formatters.js
+++ javascript/formatters.js
@@ -7,6 +7,7 @@ export function formatPokemonNumber(id) {
     return `#${String(id).padStart(5, "0")}`
 }
 
+/* makes names uppercase, because the API gives the names in lowercase */
 export function capitalize(text) {
     return text.charAt(0).toUpperCase() + text.slice(1)
 }
---
+++ javascript/pokemonCard.js
@@ -0,0 +1,32 @@
+import { getPokemonId, formatPokemonNumber, capitalize } from "./formatters";
+import { getPokemonImageUrl } from "./pokeapi";
+
+export class pokemonCard {
+    constructor(pokemon) {
+        this.name = pokemon.name
+        this.id = getPokemonId(pokemon.url)
+    }
+
+    render() {
+        const link = document.createElement("a")
+        link.className = "card"
+        link.href = `detail.html?name=${this.name}`
+
+        const number = document.createElement("span")
+        number.className = "card__number"
+        number.textContent = formatPokemonNumber(this.id)
+
+        const image = document.createElement("img")
+        image.className = "card__image"
+        image.src = getPokemonImageUrl(this.id)
+        image.alt = "this.name"
+        image.loading = "lazy"
+
+        const name = document.createElement("span")
+        name.className = "card__name"
+        name.textContent = capitalize(this.name)
+
+        link.append(number, image, name)
+        return link
+    }
+}
