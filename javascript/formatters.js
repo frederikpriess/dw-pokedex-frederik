9c83a46d110a604aed399a41fa26346eb39067e3
Fred <frederikpriesshansen@gmail.com>
2026-09-21 12:21:05 +0200

    added more text formatting in the formatter.js file

--- javascript/formatters.js
+++ javascript/formatters.js
@@ -11,3 +11,24 @@ export function formatPokemonNumber(id) {
 export function capitalize(text) {
     return text.charAt(0).toUpperCase() + text.slice(1)
 }
+
+/* added weight and height functions and divided them by 10 to get kg's and meters for the details */
+export function formatWeight(hectograms) {
+    return `${hectograms / 10} kg`
+}
+
+export function formatHeight(decimeters) {
+    return `${decimeters / 10} m`
+}
+
+/* checks if there is a english description for the pokemon */
+export function getEnglishDescription(species) {
+    const entry = species.flavor_text_entries.find(
+        (entry) => entry.language.name === "en"
+    )
+    if (!entry) {
+        return "No description available."
+    }
+    /* uses a regular expression to remove odd gaps */
+    return entry.flavor_text.replace(/\s+/g, " ")
+}
