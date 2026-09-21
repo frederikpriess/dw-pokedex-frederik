d33f5110842ebdd12b3811ecc3c48de2717ddbfc
Fred <frederikpriesshansen@gmail.com>
2026-09-21 10:29:25 +0200

    edited the limit with an extra 0, since i found out that they have made way to many pokemon now

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 import { fetchPokemonList } from "./javascript/pokeapi.js";
 const list = await fetchPokemonList()
 console.log(list)
+console.log(list.length);
+console.log(list[list.length - 1]);
+
