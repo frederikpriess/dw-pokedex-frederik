4599d28b094da36c1d241dfd44e978d9e621e4c0
Fred <frederikpriesshansen@gmail.com>
2026-09-22 10:29:24 +0200

    added svg in Header.js to get a better pokéball logo
    removed icon styling in css

--- javascript/Header.js
+++ javascript/Header.js
@@ -10,8 +10,16 @@ export class Header {
         const titleRow = document.createElement("div")
         titleRow.className = "site-header__title-row"
 
-        const icon = document.createElement("span")
+        const icon = document.createElement("div")
         icon.className = "site-header__icon"
+        icon.innerHTML = `
+    <svg viewBox="0 0 100 100" width="20" height="20">
+        <circle cx="50" cy="50" r="45" fill="white" stroke="#2c2c2c" stroke-width="6" />
+        <path d="M 5 50 A 45 45 0 0 1 95 50" fill="#dc0a2d" stroke="#2c2c2c" stroke-width="6" />
+        <line x1="5" y1="50" x2="95" y2="50" stroke="#2c2c2c" stroke-width="6" />
+        <circle cx="50" cy="50" r="12" fill="white" stroke="#2c2c2c" stroke-width="6" />
+    </svg>
+`
 
         const title = document.createElement("h1")
         title.className = " site-header__title"
