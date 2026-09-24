4fbac0b6656172cecd2887a80f51dc09ea0847e6
Fred <frederikpriesshansen@gmail.com>
2026-09-24 11:17:10 +0200

    removed observer from script, and fully removed sentinel div

--- script.js
+++ script.js
@@ -2,31 +2,18 @@ import { fetchPokemonList } from "./javascript/pokeapi.js";
 import { pokemonList } from "./javascript/pokemonList.js";
 import { Header } from "./javascript/header.js";
 import { FavoritesStore } from "./javascript/FavoritesStore.js";
-/* import { createHeartPokeballIcon } from "./javascript/icons.js"; */
 
 const pokemons = await fetchPokemonList()
 const root = document.getElementById("root")
 
 const favorites = new FavoritesStore()
-
 const list = new pokemonList(pokemons, favorites)
 
 const header = new Header((query) => {
     list.filter(query)
 })
 
-const sentinel = document.createElement("div")
-sentinel.className = "sentinel"
-
-const observer = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-        list.loadMore()
-    }
-})
-
-observer.observe(sentinel)
-
-root.append(header.render(), list.render(), sentinel)
+root.append(header.render(), list.render())
 
 
 
